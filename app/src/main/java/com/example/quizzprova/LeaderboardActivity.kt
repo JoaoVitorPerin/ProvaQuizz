aa0e1588739d8f6b4bfc4b2c90cf38c49551fa0d
JoaoVitorPerin <joao.vperin@outlook.com>
2023-06-12 21:37:51 -0300

    Adicionado animacao leaderboard

--- app/src/main/java/com/example/quizzprova/LeaderboardActivity.kt
+++ app/src/main/java/com/example/quizzprova/LeaderboardActivity.kt
@@ -1,21 +1,24 @@
 package com.example.quizzprova
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.example.quizzprova.Composable.Jogo.score
 import com.example.quizzprova.Composable.Leaderboard.LeaderboardItem
 import com.example.quizzprova.Model.Usuario
 import com.example.quizzprova.ui.theme.QuizzProvaTheme
-import kotlin.math.log
+
 
 val leaderboard = mutableListOf<Usuario>()
 
@@ -27,16 +30,54 @@ class LeaderboardActivity : ComponentActivity() {
                 Surface(
                     color = Color.Black
                 ) {
-                    Column(modifier = Modifier
-                        .background(Color.Black)
-                        .fillMaxSize()) {
-                        Text(text = "Leaderboard:",
-                        style = MaterialTheme.typography.h3,
-                        color = Color.White)
+                    Column(
+                        modifier = Modifier
+                            .background(Color.Black)
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            text = "Leaderboard:",
+                            style = MaterialTheme.typography.h3,
+                            color = Color.White
+                        )
+
                         leaderboard.sortByDescending { it.score }
-                        for ((index, usuario) in leaderboard.withIndex()) {
+                        val fadeAnimDuration = 500 // Duração da animação de fade in (em milissegundos)
+                        val fadeAnimDelayIncrement = 300 // Incremento de atraso para cada item (em milissegundos)
+                        val maxFadeAnimDelay = fadeAnimDelayIncrement * leaderboard.size
+
+                        val visibilityStates = remember {
+                            leaderboard.mapIndexed { index, _ ->
+                                mutableStateOf(index == 0)
+                            }
+                        }
+
+                        leaderboard.forEachIndexed { index, usuario ->
                             val position: Int = index
-                            LeaderboardItem(usuario, position)
+                            val fadeAnimDelay = fadeAnimDelayIncrement * position
+
+                            LaunchedEffect(key1 = visibilityStates[position].value) {
+                                visibilityStates[position].value = true
+                            }
+
+                            AnimatedVisibility(
+                                visible = visibilityStates[position].value,
+                                enter = fadeIn(
+                                    animationSpec = tween(
+                                        durationMillis = fadeAnimDuration,
+                                        delayMillis = fadeAnimDelay,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                ),
+                                exit = fadeOut(
+                                    animationSpec = tween(
+                                        durationMillis = fadeAnimDuration,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                )
+                            ) {
+                                LeaderboardItem(usuario, position)
+                            }
                         }
                     }
                 }
