f1ed4f58719267a87c927749c73cbeb360948b17
JoaoVitorPerin <joao.vperin@outlook.com>
2023-06-14 19:57:28 -0300

    App finalizado

--- app/src/main/java/com/example/quizzprova/LeaderboardActivity.kt
+++ app/src/main/java/com/example/quizzprova/LeaderboardActivity.kt
@@ -7,21 +7,22 @@ import androidx.compose.animation.*
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.quizzprova.Composable.Jogo.score
 import com.example.quizzprova.Composable.Leaderboard.LeaderboardItem
 import com.example.quizzprova.Model.Usuario
 import com.example.quizzprova.ui.theme.QuizzProvaTheme
-
 
 val leaderboard = mutableListOf<Usuario>()
 
@@ -33,53 +34,65 @@ class LeaderboardActivity : ComponentActivity() {
                 Surface(
                     color = Color.Black
                 ) {
-                    Column(
+                    Box(
                         modifier = Modifier
                             .background(Color.Black)
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                            .fillMaxSize()
+                            .padding(vertical = 16.dp)
                     ) {
-                        Text(
-                            text = "LEADERBOARD:",
-                            style = MaterialTheme.typography.h4,
-                            color = Color.Yellow
-                        )
-
-                        leaderboard.sortByDescending { it.score }
-                        val duracaoEfeitoFade = 500 // Duração da animação de fade in (em milissegundos)
-                        val incrementoEfeitoFade = 300 // Incremento de atraso para cada item (em milissegundos)
-
-                        val visibilityStates = remember {
-                            leaderboard.mapIndexed { index, _ ->
-                                mutableStateOf(index == 0)
-                            }
-                        }
-
-                        leaderboard.forEachIndexed { index, usuario ->
-                            val posicao: Int = index
-                            val fadeDelay = incrementoEfeitoFade * posicao
-
-                            LaunchedEffect(key1 = visibilityStates[posicao].value) {
-                                visibilityStates[posicao].value = true
-                            }
-
-                            AnimatedVisibility(
-                                visible = visibilityStates[posicao].value,
-                                enter = fadeIn(
-                                    animationSpec = tween(
-                                        durationMillis = duracaoEfeitoFade,
-                                        delayMillis = fadeDelay,
-                                        easing = FastOutSlowInEasing
-                                    )
-                                ),
-                                exit = fadeOut(
-                                    animationSpec = tween(
-                                        durationMillis = duracaoEfeitoFade,
-                                        easing = FastOutSlowInEasing
-                                    )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(horizontal = 16.dp)
+                                .scrollable(
+                                    orientation = Orientation.Vertical,
+                                    state = rememberScrollState()
                                 )
-                            ) {
-                                LeaderboardItem(usuario, posicao)
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            Text(
+                                text = "LEADERBOARD:",
+                                style = MaterialTheme.typography.h4,
+                                color = Color.Yellow,
+                                modifier = Modifier.padding(bottom = 16.dp)
+                            )
+
+                            leaderboard.sortByDescending { it.score }
+                            val duracaoEfeitoFade = 500 // Duração da animação de fade in (em milissegundos)
+                            val incrementoEfeitoFade = 150 // Incremento de atraso para cada item (em milissegundos)
+
+                            val visibilityStates = remember {
+                                leaderboard.mapIndexed { index, _ ->
+                                    mutableStateOf(index == 0)
+                                }
+                            }
+
+                            leaderboard.take(10).forEachIndexed { index, usuario ->
+                                val posicao: Int = index
+                                val fadeDelay = incrementoEfeitoFade * posicao
+
+                                LaunchedEffect(key1 = visibilityStates[posicao].value) {
+                                    visibilityStates[posicao].value = true
+                                }
+
+                                AnimatedVisibility(
+                                    visible = visibilityStates[posicao].value,
+                                    enter = fadeIn(
+                                        animationSpec = tween(
+                                            durationMillis = duracaoEfeitoFade,
+                                            delayMillis = fadeDelay,
+                                            easing = FastOutSlowInEasing
+                                        )
+                                    ),
+                                    exit = fadeOut(
+                                        animationSpec = tween(
+                                            durationMillis = duracaoEfeitoFade,
+                                            easing = FastOutSlowInEasing
+                                        )
+                                    )
+                                ) {
+                                    LeaderboardItem(usuario, posicao)
+                                }
                             }
                         }
                     }
@@ -88,6 +101,9 @@ class LeaderboardActivity : ComponentActivity() {
         }
     }
 }
+
+
+
 
 fun addOnLeaderboard() {
     val novoUsuario = Usuario(usuario.value, score.value)
